ace3d4eab1d9b884fbabdbc1a35af43c0f566cb1
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-22 11:55:49 +0200

    Create PlayerPawn class

---
+++ Source/D2Jam_1/Actors/PlayerPawn.cpp
@@ -0,0 +1,52 @@
+﻿// Made during D2Jam
+
+
+#include "PlayerPawn.h"
+
+#include "D2Jam_1/Components/HitPointsComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
+
+APlayerPawn::APlayerPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MovementComponent"));
+	HitPointsComponent = CreateDefaultSubobject<UHitPointsComponent>(TEXT("HitPointsComponent"));
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	SetRootComponent(Root);
+}
+
+void APlayerPawn::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+}
+
+void APlayerPawn::BeginPlay()
+{
+	OnTakeAnyDamage.AddDynamic(this, &APlayerPawn::HandleAnyDamage);
+	Super::BeginPlay();
+}
+
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void APlayerPawn::HandleAnyDamage(AActor* DamagedActor,
+                                  float Damage,
+                                  const UDamageType* DamageType,
+                                  AController* InstigatedBy,
+                                  AActor* DamageCauser)
+{
+	if (IsValid(HitPointsComponent))
+	{
+		HitPointsComponent->DecreaseHitPoints(Damage);
+	}
+}
---
+++ Source/D2Jam_1/Actors/PlayerPawn.h
@@ -0,0 +1,47 @@
+﻿// Made during D2Jam
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+class UHitPointsComponent;
+class UFloatingPawnMovement;
+
+UCLASS()
+class D2JAM_1_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	APlayerPawn();
+
+protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UFloatingPawnMovement> MovementComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UHitPointsComponent> HitPointsComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> Root = nullptr;
+
+	UFUNCTION()
+	void HandleAnyDamage(AActor* DamagedActor,
+	                     float Damage,
+	                     const UDamageType* DamageType,
+	                     AController* InstigatedBy,
+	                     AActor* DamageCauser);
+};
+
