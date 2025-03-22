5efa908a138b43294ec356b315491c2adbf8e6d3
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-22 17:51:53 +0200

    Create base StellarObject class

---
+++ Source/D2Jam_1/Actors/StellarObject.cpp
@@ -0,0 +1,47 @@
+﻿// Made during D2Jam
+
+
+#include "StellarObject.h"
+
+#include "Kismet/GameplayStatics.h"
+
+
+AStellarObject::AStellarObject()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	SetRootComponent(MeshComponent);
+}
+
+void AStellarObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MeshComponent->OnComponentBeginOverlap.AddUniqueDynamic(this, &AStellarObject::HandleBodyOverlap);
+}
+
+void AStellarObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AStellarObject::HandleBodyOverlap(UPrimitiveComponent* OverlappedComponent,
+                                       AActor* OtherActor,
+                                       UPrimitiveComponent* OtherComp,
+                                       int32 OtherBodyIndex,
+                                       bool bFromSweep,
+                                       const FHitResult& SweepResult)
+{
+	if (!IsValid(OtherActor))
+	{
+		return;
+	}
+
+	UGameplayStatics::ApplyDamage(OtherActor, ImpactDamage, nullptr, this, nullptr);
+
+	if (bDestroyOnImpact)
+	{
+		Destroy();
+	}
+}
---
+++ Source/D2Jam_1/Actors/StellarObject.h
@@ -0,0 +1,41 @@
+﻿// Made during D2Jam
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "StellarObject.generated.h"
+
+UCLASS()
+class D2JAM_1_API AStellarObject : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AStellarObject();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComponent = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "StellarObject")
+	int ImpactDamage = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "StellarObject")
+	bool bDestroyOnImpact = false;
+
+private:
+	UFUNCTION()
+	virtual void HandleBodyOverlap(UPrimitiveComponent* OverlappedComponent,
+	                               AActor* OtherActor,
+	                               UPrimitiveComponent* OtherComp,
+	                               int32 OtherBodyIndex,
+	                               bool bFromSweep,
+	                               const FHitResult& SweepResult);
+};
