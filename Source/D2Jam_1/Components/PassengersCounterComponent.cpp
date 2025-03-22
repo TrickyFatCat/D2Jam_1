0288fd339297788332c61979c5e76059ff4939fe
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-22 22:19:44 +0200

    Create PassengerCounterComponent

---
+++ Source/D2Jam_1/Components/PassengersCounterComponent.cpp
@@ -0,0 +1,41 @@
+﻿// Made during D2Jam
+
+
+#include "PassengersCounterComponent.h"
+
+#include "TrickyPropertiesLibrary.h"
+
+
+UPassengersCounterComponent::UPassengersCounterComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UPassengersCounterComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+bool UPassengersCounterComponent::IncreasePassengerCapacity(const int32 Amount)
+{
+	if (!UTrickyPropertiesLibrary::IncreaseValueInt(PassengerCapacity, Amount))
+	{
+		return false;
+	}
+
+	UTrickyPropertiesLibrary::ClampToMaxInt(PassengerCapacity);
+	return true;
+}
+
+bool UPassengersCounterComponent::IncreasePassengerMaxCapacity(const int32 Amount)
+{
+	return UTrickyPropertiesLibrary::IncreaseMaxValueInt(PassengerCapacity, Amount);
+}
+
+
+void UPassengersCounterComponent::ResetCounter()
+{
+	PassengerCapacity = DefaultPassengerCapacity;
+	TotalPassengers = 0;
+}
---
+++ Source/D2Jam_1/Components/PassengersCounterComponent.h
@@ -0,0 +1,44 @@
+﻿// Made during D2Jam
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyPropertyInt.h"
+#include "Components/ActorComponent.h"
+#include "PassengersCounterComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class D2JAM_1_API UPassengersCounterComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPassengersCounterComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Transient, Category="PassengersCounter")
+	int32 TotalPassengers = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="PassengersCounter")
+	FTrickyPropertyInt DefaultPassengerCapacity{0, 0, 10};
+
+	UFUNCTION(BlueprintGetter)
+	FTrickyPropertyInt GetPassengerCapacity() const { return PassengerCapacity; }
+
+	UFUNCTION(BlueprintCallable)
+	bool IncreasePassengerCapacity(const int32 Amount);
+
+	UFUNCTION(BlueprintCallable)
+	bool IncreasePassengerMaxCapacity(const int32 Amount);
+
+	UFUNCTION(BlueprintCallable)
+	void ResetCounter();
+
+
+private:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetPassengerCapacity, Category="PassengersCounter")
+	FTrickyPropertyInt PassengerCapacity;
+};
