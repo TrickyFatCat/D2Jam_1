6e74d9da2a87dfc92fa185be0b62a205b6330bf4
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-22 21:43:22 +0200

    Implement function to handle state chnage

--- Source/D2Jam_1/Actors/StellarObject.cpp
+++ Source/D2Jam_1/Actors/StellarObject.cpp
@@ -13,7 +13,7 @@ AStellarObject::AStellarObject()
 
 	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	SetRootComponent(Root);
-	
+
 	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
 	MeshComponent->SetupAttachment(GetRootComponent());
 
@@ -25,11 +25,48 @@ void AStellarObject::BeginPlay()
 	Super::BeginPlay();
 
 	MeshComponent->OnComponentBeginOverlap.AddUniqueDynamic(this, &AStellarObject::HandleBodyOverlap);
+	StateComponent->OnGameplayObjectStateChanged.AddUniqueDynamic(this, &AStellarObject::HandleStateChanged);
 }
 
 void AStellarObject::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+bool AStellarObject::ActivateGameplayObject_Implementation(const bool bTransitImmediately)
+{
+	return Execute_ActivateGameplayObject(StateComponent, bTransitImmediately);
+}
+
+bool AStellarObject::DeactivateGameplayObject_Implementation(const bool bTransitImmediately)
+{
+	return Execute_DeactivateGameplayObject(StateComponent, bTransitImmediately);
+}
+
+bool AStellarObject::DisableGameplayObject_Implementation(const bool bTransitImmediately)
+{
+	return Execute_DisableGameplayObject(StateComponent, bTransitImmediately);
+}
+
+bool AStellarObject::EnableGameplayObject_Implementation(const bool bTransitImmediately)
+{
+	return Execute_EnableGameplayObject(StateComponent, bTransitImmediately);
+}
+
+bool AStellarObject::ForceGameplayObjectState_Implementation(const EGameplayObjectState NewState,
+                                                             const bool bTransitImmediately)
+{
+	return Execute_ForceGameplayObjectState(StateComponent, NewState, bTransitImmediately);
+}
+
+bool AStellarObject::FinishGameplayObjetStateTransition_Implementation()
+{
+	return Execute_FinishGameplayObjetStateTransition(StateComponent);
+}
+
+bool AStellarObject::ReverseGameplayObjectStateTransition_Implementation()
+{
+	return Execute_ReverseGameplayObjectStateTransition(StateComponent);
 }
 
 void AStellarObject::HandleBodyOverlap(UPrimitiveComponent* OverlappedComponent,
@@ -50,4 +87,10 @@ void AStellarObject::HandleBodyOverlap(UPrimitiveComponent* OverlappedComponent,
 	{
 		Destroy();
 	}
+}
+
+void AStellarObject::HandleStateChanged(UGameplayObjectStateControllerComponent* Component,
+                                        EGameplayObjectState NewState,
+                                        bool bChangedImmediately)
+{
 }
--- Source/D2Jam_1/Actors/StellarObject.h
+++ Source/D2Jam_1/Actors/StellarObject.h
@@ -4,12 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayObject/GameplayObjectInterface.h"
 #include "StellarObject.generated.h"
 
 class UGameplayObjectStateControllerComponent;
 
 UCLASS()
-class D2JAM_1_API AStellarObject : public AActor
+class D2JAM_1_API AStellarObject : public AActor, public IGameplayObjectInterface
 {
 	GENERATED_BODY()
 
@@ -22,13 +23,28 @@ protected:
 public:
 	virtual void Tick(float DeltaTime) override;
 
+	virtual bool ActivateGameplayObject_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool DeactivateGameplayObject_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool DisableGameplayObject_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool EnableGameplayObject_Implementation(const bool bTransitImmediately) override;
+
+	virtual bool ForceGameplayObjectState_Implementation(const EGameplayObjectState NewState,
+	                                                     const bool bTransitImmediately) override;
+
+	virtual bool FinishGameplayObjetStateTransition_Implementation() override;
+
+	virtual bool ReverseGameplayObjectStateTransition_Implementation() override;
+
 protected:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<USceneComponent> Root = nullptr;
-	
+
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UStaticMeshComponent> MeshComponent = nullptr;
-	
+
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UGameplayObjectStateControllerComponent> StateComponent = nullptr;
 
@@ -46,4 +62,9 @@ private:
 	                               int32 OtherBodyIndex,
 	                               bool bFromSweep,
 	                               const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void HandleStateChanged(UGameplayObjectStateControllerComponent* Component,
+	                                EGameplayObjectState NewState,
+	                                bool bChangedImmediately);
 };
