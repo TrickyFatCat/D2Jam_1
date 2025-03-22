dea75876d5ec90a972ed8a15860fc00975c622a0
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-22 15:13:14 +0200

    Create function to calculate mouse position in world

---
+++ Source/D2Jam_1/JamUtils.cpp
@@ -0,0 +1,38 @@
+﻿// Made during D2Jam
+
+
+#include "JamUtils.h"
+
+#include "Kismet/KismetMathLibrary.h"
+
+
+bool UJamUtils::ProjectMouseToWorld(const APlayerController* PlayerController,
+                                    const APlayerCameraManager* CameraManager,
+                                    const float Distance,
+                                    FVector& MousePosition)
+{
+	if (!IsValid(PlayerController) || !IsValid(CameraManager))
+	{
+		MousePosition = FVector::ZeroVector;
+		return false;
+	}
+
+	FVector Location, Direction, Intersection;
+	float T = 0.f;
+
+	if (!PlayerController->DeprojectMousePositionToWorld(Location, Direction))
+	{
+		return false;
+	}
+
+	const FVector LineStart = CameraManager->GetCameraLocation();
+	const FVector LineEnd = LineStart + Direction * Distance;
+	UKismetMathLibrary::LinePlaneIntersection_OriginNormal(LineStart,
+	                                                       LineEnd,
+	                                                       FVector::ZeroVector,
+	                                                       FVector::UpVector,
+	                                                       T,
+	                                                       Intersection);
+	MousePosition = Intersection;
+	return true;
+}
---
+++ Source/D2Jam_1/JamUtils.h
@@ -0,0 +1,23 @@
+﻿// Made during D2Jam
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "JamUtils.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class D2JAM_1_API UJamUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category=JamUtils)
+	static bool ProjectMouseToWorld(const APlayerController* PlayerController,
+	                                const APlayerCameraManager* CameraManager,
+	                                const float Distance,
+	                                FVector& MousePosition);
+};
