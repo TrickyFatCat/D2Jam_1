bf7b39719564f39342f779b5c96806ccc28c8fea
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-22 20:42:16 +0200

    Create base planet actor

---
+++ Source/D2Jam_1/Actors/PlanetBase.cpp
@@ -0,0 +1,50 @@
+﻿// Made during D2Jam
+
+
+#include "PlanetBase.h"
+
+#include "TrickyGameModeLibrary.h"
+#include "Components/SphereComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+APlanetBase::APlanetBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerComponent = CreateDefaultSubobject<USphereComponent>(TEXT("TriggerComponent"));
+	TriggerComponent->SetupAttachment(MeshComponent);
+}
+
+void APlanetBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	InitialLocation = GetActorLocation();
+}
+
+void APlanetBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlanetBase::SetPlanetColor(const EPlanetColor NewPlanetColor, const FLinearColor NewColor)
+{
+	PlanetColor = NewPlanetColor;
+	Color = NewColor;
+}
+
+void APlanetBase::RandomizePlanet()
+{
+	if (RandomizationRadius > 0.f)
+	{
+		FVector Direction = UKismetMathLibrary::RandomUnitVector();
+		Direction.Z = 0.f;
+		Direction.Normalize();
+		const FVector NewLocation = InitialLocation + Direction * UKismetMathLibrary::RandomFloatInRange(0.f, RandomizationRadius);
+		SetActorLocation(NewLocation);
+	}
+	
+	HandlePlanetRandomized();
+}
+
---
+++ Source/D2Jam_1/Actors/PlanetBase.h
@@ -0,0 +1,51 @@
+﻿// Made during D2Jam
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StellarObject.h"
+#include "D2Jam_1/Misc/PlanetColors.h"
+#include "PlanetBase.generated.h"
+
+class USphereComponent;
+class UGameplayObjectStateControllerComponent;
+
+UCLASS()
+class D2JAM_1_API APlanetBase : public AStellarObject
+{
+	GENERATED_BODY()
+
+public:
+	APlanetBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	void SetPlanetColor(EPlanetColor NewPlanetColor, FLinearColor NewColor);
+
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	void RandomizePlanet();
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USphereComponent> TriggerComponent = nullptr;
+	
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Planet")
+	EPlanetColor PlanetColor = EPlanetColor::Red;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Planet")
+	FLinearColor Color = FColor::Magenta;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "Planet")
+	FVector InitialLocation = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Planet")
+	float RandomizationRadius = 200.0f;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Planet")
+	void HandlePlanetRandomized();
+};
