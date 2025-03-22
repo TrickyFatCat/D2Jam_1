48045c8e4797a5270078a57ba5ec97122ee3405d
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-22 22:05:27 +0200

    Create PassengerGeneratorComponent

---
+++ Source/D2Jam_1/Components/PassengersGeneratorComponent.cpp
@@ -0,0 +1,32 @@
+﻿// Made during D2Jam
+
+
+#include "PassengersGeneratorComponent.h"
+
+
+UPassengersGeneratorComponent::UPassengersGeneratorComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UPassengersGeneratorComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	PassengerCapacity = DefaultPassengerCapacity;
+}
+
+void UPassengersGeneratorComponent::StartGenerator()
+{
+}
+
+void UPassengersGeneratorComponent::StopGenerator()
+{
+}
+
+void UPassengersGeneratorComponent::RestartGenerator()
+{
+	PassengerCapacity = DefaultPassengerCapacity;
+	StartGenerator();
+}
---
+++ Source/D2Jam_1/Components/PassengersGeneratorComponent.h
@@ -0,0 +1,54 @@
+﻿// Made during D2Jam
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrickyPropertyInt.h"
+#include "Components/ActorComponent.h"
+#include "D2Jam_1/Misc/PlanetColors.h"
+#include "PassengersGeneratorComponent.generated.h"
+
+
+enum class EPlanetColor : uint8;
+class UPlanetColors;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class D2JAM_1_API UPassengersGeneratorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPassengersGeneratorComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="PassengersGenerator")
+	FTrickyPropertyInt DefaultPassengerCapacity{0, 0, 12};
+
+	UFUNCTION()
+	void SetPlanetColors(UPlanetColors* Value) { PlanetColors = Value; }
+
+	UFUNCTION()
+	void SetExcludedColor(const EPlanetColor Value) { ExcludedColor = Value; }
+
+	UFUNCTION()
+	void StartGenerator();
+
+	UFUNCTION()
+	void StopGenerator();
+
+	UFUNCTION()
+	void RestartGenerator();
+	
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="PassengersGenerator")
+	FTrickyPropertyInt PassengerCapacity;
+
+	UPROPERTY(VisibleInstanceOnly, Category="PassengersGenerator")
+	UPlanetColors* PlanetColors = nullptr;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="PassengersGenerator")
+	EPlanetColor ExcludedColor = EPlanetColor::Red;
+};
