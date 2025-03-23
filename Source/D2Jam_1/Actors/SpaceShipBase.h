3f7c08cad6e958de95d2fe003d733786ffb85927
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-23 20:27:51 +0200

    Create SpaceShipBase class

---
+++ Source/D2Jam_1/Actors/SpaceShipBase.h
@@ -0,0 +1,28 @@
+﻿// Made during D2Jam
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StellarObject.h"
+#include "SpaceShipBase.generated.h"
+
+class UFloatingPawnMovement;
+
+UCLASS()
+class D2JAM_1_API ASpaceShipBase : public AStellarObject
+{
+	GENERATED_BODY()
+
+public:
+	ASpaceShipBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UFloatingPawnMovement> MovementComponent = nullptr;
+};
