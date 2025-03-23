249a017f6b0bbb17b0bc135ff081310f225018f2
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-03-23 08:57:07 +0200

    Implement function which adds a random passenger

--- Source/D2Jam_1/Components/PassengersGeneratorComponent.cpp
+++ Source/D2Jam_1/Components/PassengersGeneratorComponent.cpp
@@ -3,6 +3,10 @@
 
 #include "PassengersGeneratorComponent.h"
 
+#include "D2Jam_1/Actors/PlanetsManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
 
 UPassengersGeneratorComponent::UPassengersGeneratorComponent()
 {
@@ -15,6 +19,20 @@ void UPassengersGeneratorComponent::InitializeComponent()
 	Super::InitializeComponent();
 
 	PassengerCapacity = DefaultPassengerCapacity;
+
+	TArray<AActor*> Actors;
+	UGameplayStatics::GetAllActorsOfClass(this, APlanetsManager::StaticClass(), Actors);
+}
+
+bool UPassengersGeneratorComponent::IncrementPossibleColors()
+{
+	if (CurrentPossibleColors >= MaxPossibleColors)
+	{
+		return false;
+	}
+
+	CurrentPossibleColors++;
+	return true;
 }
 
 void UPassengersGeneratorComponent::StartGenerator()
@@ -29,4 +47,47 @@ void UPassengersGeneratorComponent::RestartGenerator()
 {
 	PassengerCapacity = DefaultPassengerCapacity;
 	StartGenerator();
+}
+
+bool UPassengersGeneratorComponent::IncreaseCapacity(const int32 Amount)
+{
+	if (Amount <= 0)
+	{
+		return false;
+	}
+
+	PassengerCapacity += Amount;
+	return true;
+}
+
+bool UPassengersGeneratorComponent::AddRandomPassenger()
+{
+	if (Passengers.Num() >= PassengerCapacity)
+	{
+		return false;
+	}
+
+	TArray<EPlanetColor> PassengerColors;
+	PlanetColors->Colors.GetKeys(PassengerColors);
+	PassengerColors.Remove(ExcludedColor);
+	
+	int32 ColorIndex = 0;
+	int32 Counter = 0;
+	
+	do
+	{
+		ColorIndex = UKismetMathLibrary::RandomIntegerInRange(0, CurrentPossibleColors - 1);
+		Counter++;
+	}
+	while (!PassengerColors.IsValidIndex(ColorIndex) || Counter < 100);
+
+	if (!PassengerColors.IsValidIndex(ColorIndex))
+	{
+		ColorIndex = 0;
+	}
+	
+	const EPlanetColor PassengerColor = PassengerColors[ColorIndex];
+	Passengers.Add(PassengerColor);
+	OnNewPassengerAdded.Broadcast(this, PassengerColor);
+	return true;
 }
--- Source/D2Jam_1/Components/PassengersGeneratorComponent.h
+++ Source/D2Jam_1/Components/PassengersGeneratorComponent.h
@@ -3,14 +3,16 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "TrickyPropertyInt.h"
 #include "Components/ActorComponent.h"
 #include "D2Jam_1/Misc/PlanetColors.h"
 #include "PassengersGeneratorComponent.generated.h"
 
-
 enum class EPlanetColor : uint8;
 class UPlanetColors;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnNewPassengerAddedDynamicSignature,
+                                             UPassengersGeneratorComponent*, Component,
+                                             EPlanetColor, PassengerColor);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class D2JAM_1_API UPassengersGeneratorComponent : public UActorComponent
@@ -24,8 +26,23 @@ protected:
 	virtual void InitializeComponent() override;
 
 public:
+	UPROPERTY(BlueprintAssignable)
+	FOnNewPassengerAddedDynamicSignature OnNewPassengerAdded;
+	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="PassengersGenerator")
-	FTrickyPropertyInt DefaultPassengerCapacity{0, 0, 12};
+	int32 DefaultPassengerCapacity = 12;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="PassengersGenerator")
+	int32 MaxPossibleColors = 3;
+
+	UFUNCTION()
+	bool IncrementPossibleColors();
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetPassengerCapacity() const { return PassengerCapacity; }
+
+	UFUNCTION(BlueprintGetter)
+	TArray<EPlanetColor> GetPassengers() const { return Passengers; }
 
 	UFUNCTION()
 	void SetPlanetColors(UPlanetColors* Value) { PlanetColors = Value; }
@@ -41,14 +58,29 @@ public:
 
 	UFUNCTION()
 	void RestartGenerator();
-	
+
+	UFUNCTION(BlueprintCallable, Category="PassengersGenerator")
+	bool IncreaseCapacity(const int32 Amount);
+
 protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetPassengerCapacity, Category="PassengersGenerator")
+	int32 PassengerCapacity = 12;
+
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="PassengersGenerator")
-	FTrickyPropertyInt PassengerCapacity;
+	int32 CurrentPossibleColors = 2;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetPassengers, Category="PassengersGenerator")
+	TArray<EPlanetColor> Passengers;
 
 	UPROPERTY(VisibleInstanceOnly, Category="PassengersGenerator")
 	UPlanetColors* PlanetColors = nullptr;
 
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="PassengersGenerator")
 	EPlanetColor ExcludedColor = EPlanetColor::Red;
+
+	UPROPERTY()
+	FTimerHandle GenerationTimerHandle;
+
+	UFUNCTION(BlueprintCallable)
+	bool AddRandomPassenger();
 };
